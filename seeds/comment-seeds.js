8784fdc6d1cc9c43f9a5244c67ef912ad1292af1
bennettem <emily.pamelabennett@gmail.com>
2022-07-21 01:09:56 -0500

    controller/api index added and comment seeds start

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,12 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        user_id: 1,
+        post_id: 2,
+        comment_text: "Super stoked about this application!"
+    },
+    {
+        
+    }
+]
