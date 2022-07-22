53377814e1a62cc50b76b40ee16ebd1b19360ca8
bennettem <emily.pamelabennett@gmail.com>
2022-07-21 22:06:48 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,8 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      dialect: 'mysql',
+      port: 3306
     });
 
 module.exports = sequelize;
