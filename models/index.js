010365ed62252b409e6ce4be4d1a152b4b610599
bennettem <emily.pamelabennett@gmail.com>
2022-07-21 22:38:35 -0500

    heroku

--- models/index.js
+++ models/index.js
@@ -2,33 +2,31 @@ const Comment = require("./Comment")
 const Post = require("./Post");
 const User = require("./User");
 
-Comment.belongsTo(User, {
-    foreignKey: "post_id",
-    onDelete: "SET NULL"
+//user
+User.hasMany(Post, {
+    foreignKey: 'user_id'
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: "user_id",
-    onDelete: "SET NULL"
-});
 
+
+//post
 Post.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE"
+    foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: "CASCADE"
+    foreignKey: 'post_id'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
 
-User.hasMany(Post, {
+
+//comment
+Comment.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 
