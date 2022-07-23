1a3e616680af869672993a834da0502dffaf10ee
bennettem <emily.pamelabennett@gmail.com>
2022-07-23 09:39:01 -0500

    heroku

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,52 +3,42 @@ const { User } = require('../models');
 const userdata = [
   {
     username: 'spookyworm',
-    email: 'spookyworm@cbc.ca',
     password: 'password123'
   },
   {
-    username: 'slipperynoodle',
-    email: 'slippernoodle@sogou.com',
+    username: 'slipperynoodle',,
     password: 'password123'
   },
   {
     username: 'bunnyhead',
-    email: 'bunnyhead@last.fm',
     password: 'password123'
   },
   {
     username: 'mrpickles',
-    email: 'pickles@goo.ne.jp',
     password: 'password123'
   },
   {
     username: 'groovycat',
-    email: 'catgrooves@weather.com',
     password: 'password123'
   },
   {
     username: 'bigfish',
-    email: 'bigfish@imdb.com',
     password: 'password123'
   },
   {
     username: 'muffinman',
-    email: 'muffinman@feedburner.com',
     password: 'password123'
   },
   {
     username: 'kitkatlover',
-    email: 'kklover2@china.com.cn',
     password: 'password123'
   },
   {
     username: 'crispychickenhead',
-    email: 'chicken8@google.ru',
     password: 'password123'
   },
   {
     username: 'nuggetmcgee',
-    email: 'nuggies@epa.gov',
     password: 'password123'
   }
 ];
